0fb9fc1a81c0ead4b8dc46dadbfe471f90dd2632
Jonatan Lemes <jonatan@jslsolucoes.com>
2025-02-10 21:08:15 -0300

    bump versions to 0.1.12 and 0.1.1; refactor main function to accept project folder and dependencies

--- Makefile
+++ Makefile
@@ -5,5 +5,11 @@ format:
 	@cargo fmt
 lint:
 	@cargo clippy
-publish: test format lint
-	@cargo run next-release && cargo publish --allow-dirty -p axum-webtools && cargo publish --allow-dirty -p axum-webtools-macros
+pre-checks: test format lint
+	@echo "All pre-checks passed"
+publish-axum-webtools:
+	@cargo run next-release tools && cargo publish --allow-dirty -p axum-webtools
+publish-axum-webtools-macros:
+	@cargo run next-release macros axum-webtools && cargo publish --allow-dirty -p axum-webtools-macros
+publish-all: pre-checks publish-axum-webtools publish-axum-webtools-macros
+	@echo "Published all packages"
--- next-release/src/main.rs
+++ next-release/src/main.rs
@@ -3,16 +3,6 @@ use std::path::Path;
 use toml_edit::DocumentMut;
 
 #[derive(Deserialize)]
-struct WorkspaceCargoWorkspace {
-    members: Vec<String>,
-}
-
-#[derive(Deserialize)]
-struct WorkspaceCargo {
-    workspace: WorkspaceCargoWorkspace,
-}
-
-#[derive(Deserialize)]
 struct BinCargoPackage {
     version: String,
 }
@@ -32,38 +22,29 @@ where
 }
 
 fn main() {
-    let workspace_file = Path::new("Cargo.toml");
-    let (workspace_cargo, _) = parse::<WorkspaceCargo>(workspace_file);
-    for member in workspace_cargo
-        .workspace
-        .members
-        .iter()
-        .map(|m| m.as_str())
-        .filter(|m| *m != "next-release")
-    {
-        let cargo_file = Path::new(&member).join("Cargo.toml");
-        let (bin_cargo, cargo_file_contents) = parse::<BinCargo>(&cargo_file);
-        let current_version = bin_cargo
-            .package
-            .version
-            .parse::<semver::Version>()
-            .unwrap();
-        let next_version = semver::Version {
-            major: current_version.major,
-            minor: current_version.minor,
-            patch: current_version.patch + 1,
-            pre: Default::default(),
-            build: Default::default(),
-        };
-        let mut doc = cargo_file_contents
-            .parse::<DocumentMut>()
-            .expect("invalid doc");
-        doc["package"]["version"] = toml_edit::value(next_version.to_string().to_string());
+    let project_folder = std::env::args().nth(2).expect("project folder is required");
+    let other_deps = std::env::args().nth(3);
+    let cargo_file = Path::new(&project_folder).join("Cargo.toml");
+    let (bin_cargo, cargo_file_contents) = parse::<BinCargo>(&cargo_file);
+    let current_version = bin_cargo
+        .package
+        .version
+        .parse::<semver::Version>()
+        .unwrap();
+    let next_version = semver::Version {
+        major: current_version.major,
+        minor: current_version.minor,
+        patch: current_version.patch + 1,
+        pre: Default::default(),
+        build: Default::default(),
+    };
+    let mut doc = cargo_file_contents
+        .parse::<DocumentMut>()
+        .expect("invalid doc");
+    doc["package"]["version"] = toml_edit::value(next_version.to_string().to_string());
+    std::fs::write(&cargo_file, doc.to_string()).expect("failed to write file");
+    if let Some(dep) = other_deps {
+        doc["dependencies"][dep] = toml_edit::value(next_version.to_string().to_string());
         std::fs::write(&cargo_file, doc.to_string()).expect("failed to write file");
-        if member == "macros" {
-            doc["dependencies"]["axum-webtools"] =
-                toml_edit::value(next_version.to_string().to_string());
-            std::fs::write(&cargo_file, doc.to_string()).expect("failed to write file");
-        }
     }
 }
