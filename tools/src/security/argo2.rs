bb66e488757c0bb13b08d740b4d8f34d144347cb
Jonatan Lemes <jonatan@jslsolucoes.com>
2026-04-08 17:10:19 -0300

    feat: add logging for invalid Argon2 password hash format and update dependencies

--- tools/src/security/argo2.rs
+++ tools/src/security/argo2.rs
@@ -1,5 +1,6 @@
 use argon2::password_hash::Error;
 use argon2::{Argon2, PasswordHash, PasswordHasher, PasswordVerifier};
+use log::warn;
 
 pub fn argo2_gen_salt_string() -> Result<[u8; 16], Error> {
     let mut buf = [0u8; 16];
@@ -17,6 +18,9 @@ pub fn argo2_gen_password(password: &str) -> Result<String, Error> {
 
 pub fn argo2_verify_password(password_hash: &str, password: &str) -> Result<bool, Error> {
     let password = password.as_bytes();
+    if !password_hash.starts_with("$argon") {
+        warn!("Password hash does not seem to be an argon2 hash. Check if you have swapped password_hash and password parameters.");
+    }
     let pwd_hash = PasswordHash::new(password_hash)?;
     let argon2 = Argon2::default();
     match argon2.verify_password(password, &pwd_hash) {
@@ -47,5 +51,13 @@ mod tests {
         let password_hash = "$argon2id$v=19$m=19456,t=2,p=1$wijorMW9eKJS9TlAw/Ay1w$Pl7feMYupjerbKpS563kFwdi9K3PdcjUnx5f4pNEbjI";
         let password = "12345678";
         assert_eq!(argo2_verify_password(password_hash, password).unwrap(), true);
+    }
+
+    #[test]
+    fn test_invalid_hash_format() {
+        env_logger::init();
+        let password_hash = "invalid_hash_format";
+        let password = "12345678";
+        assert!(argo2_verify_password(password_hash, password).is_err());
     }
 }
